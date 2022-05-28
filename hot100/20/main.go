96e92c5b8931544a2393c2e826e8e07ecf1ab659
chuang <chuangspace@126.com>
2022-05-28 10:41:17 +0800

    合并2个升序列表

--- hot100/20/main.go
+++ hot100/20/main.go
@@ -1,1 +1,56 @@
-package _0
+/**
+给定一个只包括 '('，')'，'{'，'}'，'['，']'的字符串 s ，判断字符串是否有效。
+
+有效字符串需满足：
+
+左括号必须用相同类型的右括号闭合。
+左括号必须以正确的顺序闭合。
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/valid-parentheses
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+示例 2：
+
+输入：s = "()[]{}"
+输出：true
+示例 3：
+
+输入：s = "(]"
+输出：false
+
+*/
+
+package main
+
+import (
+	"fmt"
+)
+
+func isValid(s string) bool {
+	if len(s)%2 == 1 {
+		return false
+	}
+	pairs := map[byte]byte{
+		')': '(',
+		'}': '{',
+		']': '[',
+	}
+	stack := []byte{}
+	for i := 0; i < len(s); i++ {
+		if pairs[s[i]] > 0 {
+			if len(stack) == 0 || stack[len(stack)-1] != pairs[s[i]] {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		} else {
+			stack = append(stack, s[i])
+		}
+	}
+	return len(stack) == 0
+}
+
+func main() {
+	s := "([])"
+	fmt.Println(isValid(s))
+}
